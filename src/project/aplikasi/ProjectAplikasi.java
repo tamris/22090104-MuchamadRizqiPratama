7a9f5c20e431e80f8f83a334433689e89122113b
tamris <113833655+tamris@users.noreply.github.com>
2023-12-21 10:32:23 +0700

    second commit

--- src/project/aplikasi/ProjectAplikasi.java
+++ src/project/aplikasi/ProjectAplikasi.java
@@ -15,6 +15,9 @@ public class ProjectAplikasi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login LoginFrame =new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
     }
-    
 }
